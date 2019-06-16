bf325ae6676c701ee7dca1ea83b1fa233e069c0a
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-06-15 20:41:56 -0700

    work WIP
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/pages/work.js
+++ src/pages/work.js
@@ -4,6 +4,7 @@ import { Card, StyledBody, StyledAction } from 'baseui/card';
 import { Button } from 'baseui/button';
 import * as T from 'baseui/typography';
 import { Block } from 'baseui/block';
+import { FlexGrid, FlexGridItem } from 'baseui/flex-grid';
 
 import Layout from '../components/layout';
 import Container from '../components/container';
@@ -30,36 +31,43 @@ export default () => {
         }
     `);
 
-    const test = [0, 1, 2, 3, 4, 5];
+    const test = [1, 2, 3, 4, 5, 6];
 
     return (
         <Layout>
             <T.H2 $style={{ textAlign: 'center' }}>My Work</T.H2>
 
             <Container width="1200px" $style={{ border: '3px solid red' }}>
-                <Block $style={{ border: '3px solid magenta' }}>
+                <FlexGrid
+                    $style={{ border: '3px solid magenta' }}
+                    padding="1rem"
+                    flexGridColumnCount={[1, 1, 2, 3]}
+                    flexGridColumnGap="scale800"
+                    flexGridRowGap="scale800"
+                >
                     {/* {data.allContentfulProject.edges.map((edge, i) => ( */}
                     {test.map((t, i) => (
-                        <Card
-                            key={t}
-                            headerImage={
-                                'https://source.unsplash.com/user/erondu/700x400'
-                            }
-                            title={t}
-                        >
-                            <StyledBody>
-                                Proin ut dui sed metus pharetra hend rerit vel
-                                non mi. Nulla ornare faucibus ex, non facilisis
-                                nisl.
-                            </StyledBody>
-                            <StyledAction>
-                                <Button style={{ width: '100%' }}>
-                                    Button Label
-                                </Button>
-                            </StyledAction>
-                        </Card>
+                        <FlexGridItem key={t}>
+                            <Card
+                                headerImage={
+                                    'https://source.unsplash.com/user/erondu/700x400'
+                                }
+                                title={t}
+                            >
+                                <StyledBody>
+                                    Proin ut dui sed metus pharetra hend rerit
+                                    vel non mi. Nulla ornare faucibus ex, non
+                                    facilisis nisl.
+                                </StyledBody>
+                                <StyledAction>
+                                    <Button style={{ width: '100%' }}>
+                                        Button Label
+                                    </Button>
+                                </StyledAction>
+                            </Card>
+                        </FlexGridItem>
                     ))}
-                </Block>
+                </FlexGrid>
             </Container>
         </Layout>
     );
