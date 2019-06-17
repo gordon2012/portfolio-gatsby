01455359d13cdba1f9f2689f1ea6a77099a0a406
Gordon Doskas <gordon2012@users.noreply.github.com>
2019-06-17 00:33:08 -0700

    Work page WIP
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/pages/work.js
+++ src/pages/work.js
@@ -5,7 +5,10 @@ import { Button } from 'baseui/button';
 import * as T from 'baseui/typography';
 import { Block } from 'baseui/block';
 import { FlexGrid, FlexGridItem } from 'baseui/flex-grid';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { Tag } from 'baseui/tag';
+import Search from 'baseui/icon/search';
+
+// import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import Layout from '../components/layout';
 import Container from '../components/container';
@@ -37,18 +40,11 @@ export default () => {
             }
         }
     `);
-    // const { title, category, skills, images, links, body } = data
-
     const nodes = data.allContentfulProject.edges.map(edge => edge.node);
-
-
-    const test = [1, 2, 3, 4, 5, 6];
 
     return (
         <Layout>
             <T.H2 $style={{ textAlign: 'center' }}>My Work</T.H2>
-
-            <pre>{JSON.stringify(nodes, undefined, 2)}</pre>
 
             <Container width="1200px" $style={{ border: '3px solid red' }}>
                 <FlexGrid
@@ -59,41 +55,54 @@ export default () => {
                     flexGridRowGap="2rem"
                 >
                     {nodes.map((node, i) => (
-                    
                         <FlexGridItem key={i}>
                             <Card
                                 headerImage={node.images[0].file.url}
                                 title={node.title}
+                                overrides={{
+                                    Contents: {
+                                        style: {
+                                            marginTop: '0.2rem',
+                                        },
+                                    },
+                                }}
                             >
                                 <StyledBody>
-                                    {documentToReactComponents(node.body.json)}
+                                    <Block
+                                        marginLeft="-5px"
+                                        marginBottom="0.2rem"
+                                    >
+                                        <Tag
+                                            $as="div"
+                                            closeable={false}
+                                            kind="neutral"
+                                            variant="outlined"
+                                        >
+                                            {node.category}
+                                        </Tag>
+                                    </Block>
+
+                                    <Block marginLeft="-5px">
+                                        {node.skills.map((skill, i) => (
+                                            <Tag
+                                                key={i}
+                                                closeable={false}
+                                                kind="neutral"
+                                                variant="solid"
+                                            >
+                                                {skill}
+                                            </Tag>
+                                        ))}
+                                    </Block>
                                 </StyledBody>
                                 <StyledAction>
-
-                                    
-
-
-                                    {/* {node.links.map((link, i, a) => (
-                                        <Button style={{ width: `${a.length > 1 ? '50' : '100'}%` }}>
-                                            Button Label
-                                        </Button>
-                                    ))} */}
-
-                                    {node.links.length > 1 ? (
-                                        <>
-                                            <Button style={{ width: '50%', marginRight: '1rem' }}>
-                                                {node.links[0].title}
-                                            </Button>
-                                            <Button style={{ width: '50%', marginLeft: '1rem' }}>
-                                                {node.links[1].title}
-                                            </Button>
-                                        </>
-                                    ) : (
-                                        <Button style={{ width: '100%' }}>
-                                            {node.links[0].title}
-                                        </Button>
-                                    )}
-                                    
+                                    <Button
+                                        size="compact"
+                                        kind="primary"
+                                        style={{ width: `100%` }}
+                                    >
+                                        <Search size={48} />
+                                    </Button>
                                 </StyledAction>
                             </Card>
                         </FlexGridItem>
