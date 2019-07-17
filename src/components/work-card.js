5f1f328ceef9c4a0b4cc65aed6e038e2cababebe
gordon <gordon2012@users.noreply.github.com>
2019-07-16 22:38:49 -0700

    Work page main structure
    Signed-off-by: gordon <gordon2012@users.noreply.github.com>

--- src/components/work-card.js
+++ src/components/work-card.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
-import { Card, StyledBody, StyledAction } from 'baseui/card';
+import { Card } from 'baseui/card';
 import { Button } from 'baseui/button';
 import * as T from 'baseui/typography';
 import { Block } from 'baseui/block';
-import { FlexGrid, FlexGridItem } from 'baseui/flex-grid';
-import { Tag, VARIANT } from 'baseui/tag';
+import { Tag } from 'baseui/tag';
 import Search from 'baseui/icon/search';
 
 import { styled } from 'baseui';
-
-// console.log(VARIANT);
 
 const Image = styled('img', {});
 
@@ -28,86 +25,49 @@ const WorkCard = ({ work }) => {
                 },
             }}
         >
-            <Block paddingBottom="0.5rem" $style={{ textAlign: 'center' }}>
-                <h2>{work.title}</h2>
+            <Block paddingBottom="0.5rem" $style={{
+                textAlign: 'center',
+                backgroundColor: '#333',
+                borderBottom: '1px solid white',
+            }}>
+                <T.H5 color="white" margin="0" padding="1rem 0">
+                    {work.title}
+                </T.H5>
                 {work.skills.map(skill => (
                     <Tag
                         key={skill}
                         closeable={false}
-                        kind="neutral"
-                        variant="light"
+                        kind="negative"
+                        variant="solid"
                     >
                         {skill}
                     </Tag>
                 ))}
             </Block>
-            <Image
-                src={work.images[0].file.url}
-                margin="1rem"
-                $style={{
-                    objectFit: 'contain',
-                    maxWidth: '100%',
-                    display: 'block',
-                }}
-            />
+            <Block padding="0.5rem">
+                <Image
+                    src={work.images[0].file.url}
+                    $style={{
+                        objectFit: 'contain',
+                        maxWidth: '100%',
+                        display: 'block',
+                    }}
+                />
+            </Block>
             <Block
                 padding="0"
                 marginBottom="-16px"
-                $style={{ borderTop: '1px solid #ccc' }}
             >
                 <Button
                     kind="minimal"
-                    $style={{ width: '100%', color: '#333' }}
+                    $style={{
+                        width: '100%',
+                        color: '#333'
+                    }}
                 >
                     <Search size={36} title="View" />
                 </Button>
             </Block>
-        </Card>
-    );
-
-    return (
-        <Card
-            headerImage={work.images[0].file.url}
-            title={work.title}
-            overrides={{
-                Contents: {
-                    style: {
-                        // border: '3px solid red',
-                        marginTop: '0.2rem',
-                    },
-                },
-            }}
-        >
-            <StyledBody>
-                <Block marginLeft="-5px" marginBottom="0.2rem">
-                    <Tag
-                        $as="div"
-                        closeable={false}
-                        kind="neutral"
-                        variant="outlined"
-                    >
-                        {work.category}
-                    </Tag>
-                </Block>
-
-                <Block marginLeft="-5px">
-                    {work.skills.map((skill, i) => (
-                        <Tag
-                            key={i}
-                            closeable={false}
-                            kind="neutral"
-                            variant="solid"
-                        >
-                            {skill}
-                        </Tag>
-                    ))}
-                </Block>
-            </StyledBody>
-            <StyledAction>
-                <Button size="compact" kind="primary" style={{ width: `100%` }}>
-                    <Search size={48} />
-                </Button>
-            </StyledAction>
         </Card>
     );
 };
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
-import { Card, StyledBody, StyledAction } from 'baseui/card';
-import { Button } from 'baseui/button';
 import * as T from 'baseui/typography';
-import { Block } from 'baseui/block';
 import { FlexGrid, FlexGridItem } from 'baseui/flex-grid';
-import { Tag } from 'baseui/tag';
-import Search from 'baseui/icon/search';
 
 // import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import Layout from '../components/layout';
 import Container from '../components/container';
-
 import WorkCard from '../components/work-card';
 
 export default () => {
@@ -44,28 +38,36 @@ export default () => {
     `);
     const nodes = data.allContentfulProject.edges.map(edge => edge.node);
 
+    const categories = Array.from(new Set(nodes.map(node => node.category)));
+
+    let projects = {};
+    categories.forEach(category => {
+        projects[category] = nodes.filter(node => node.category === category);
+    });
+
     return (
         <Layout backgroundColor="#ddd">
             <T.H2 $style={{ textAlign: 'center' }}>My Work</T.H2>
 
-            <pre>
-                <code>{JSON.stringify(nodes, null, 2)}</code>
-            </pre>
-
-            <Container width="1200px" $style={{ border: '0px solid red' }}>
-                <FlexGrid
-                    $style={{ border: '0px solid magenta' }}
-                    padding="2rem"
-                    flexGridColumnCount={[1, 1, 2, 3]}
-                    flexGridColumnGap="2rem"
-                    flexGridRowGap="2rem"
-                >
-                    {nodes.map((node, i) => (
-                        <FlexGridItem key={i}>
-                            <WorkCard work={node} />
-                        </FlexGridItem>
-                    ))}
-                </FlexGrid>
+            <Container width="1200px" padding="0 2rem 2rem" $style={{ border: '0px solid red' }}>
+                {categories.map(category => (
+                    <React.Fragment key={category}>
+                        <T.H4>{category}</T.H4>
+                        <FlexGrid
+                            $style={{ border: '0px solid magenta' }}
+                            padding="0rem"
+                            flexGridColumnCount={[1, 1, 2, 3]}
+                            flexGridColumnGap="2rem"
+                            flexGridRowGap="2rem"
+                        >
+                            {projects[category].map((project, i) => (
+                                <FlexGridItem key={i}>
+                                    <WorkCard work={project} />
+                                </FlexGridItem>
+                            ))}
+                        </FlexGrid>
+                    </React.Fragment>
+                ))}
             </Container>
         </Layout>
     );
