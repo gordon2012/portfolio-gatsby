a8efc18a9750a25be99a38c99062caed45167e1e
Gordon Doskas <gordon2012@users.noreply.github.com>
2020-01-22 23:28:57 -0700

    styled to work WIP
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,15 +1,79 @@
 import 'array-flat-polyfill';
 import React, { useState } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
-import * as T from 'baseui/typography';
-import { Block } from 'baseui/block';
-import { Tag } from 'baseui/tag';
-import { Card } from 'baseui/card';
+// import * as T from 'baseui/typography';
+// import { Block } from 'baseui/block';
+// import { Tag } from 'baseui/tag';
+// import { Card } from 'baseui/card';
 
 import Layout from '../components/layout';
-import Container from '../components/container';
+// import Container from '../components/container';
 import WorkCard from '../components/work-card';
 import WorkModal from '../components/work-modal';
+
+
+import styled from 'styled-components';
+
+const Background = styled.div`
+    flex: 1;
+    background: #ddd;
+`;
+
+const Container = styled.div`
+    max-width: 1200px;
+    width: 100%;
+    margin: 0 auto;
+`;
+
+
+
+const Card = styled.div`
+
+    background: white;
+    padding: 2rem;
+
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.16);
+
+`;
+
+const Tag = ({ children }) => {
+    const Outer = styled.span`
+
+        background: red;
+        color: white;
+
+        padding: 2px 12px;
+        margin: 5px;
+        border-radius: 12px;
+        height: 24px;
+    `;
+    const Inner = styled.span`
+        font-weight: bold;
+        text-align: center;
+    `;
+    return (
+        <Outer>
+            <Inner>
+                {children}
+            </Inner>
+        </Outer>
+    );
+};
+
+
+const OldTag = styled.span`
+    /* background: red;
+    color: white; */
+
+    padding: 2px 12px;
+    margin: 5px;
+    border-radius: 12px;
+    height: 24px;
+
+`;
+
+
+
 
 const WorkItem = ({ work, filter, setFilter }) => {
     const [isOpen, setOpen] = useState(false);
@@ -64,6 +128,7 @@ const WorkPage = () => {
             }
         }
     `);
+    // console.log(data);
 
     const categories = data.allContentfulProject.group.map(group => {
         return {
@@ -74,6 +139,7 @@ const WorkPage = () => {
             }),
         };
     });
+    console.log(categories);
 
     const skills = Array.from(
         new Set(
@@ -84,96 +150,112 @@ const WorkPage = () => {
                 .flat(2)
         )
     ).sort();
+    // console.log(skills);
 
     const [filter, setFilter] = useState('');
 
+
     return (
-        <Layout backgroundColor="#ddd">
-            <T.H2 $style={{ textAlign: 'center' }}>My Work</T.H2>
+        <Layout>
+            <Background>
+                <Container>
+                    <h2>My Work</h2>
 
-            <Container width="1200px" padding="0 2rem">
-                <Card>
-                    <T.H5>Filter by Skill:</T.H5>
-                    <Block
-                        $style={{
-                            textAlign: 'center',
-                            border: '0px solid red',
-                        }}
-                    >
-                        <Tag
-                            closeable={false}
-                            kind="primary"
-                            variant={filter === '' ? 'solid' : 'outlined'}
-                            onClick={filter === '' ? null : () => setFilter('')}
-                            isFocused
-                        >
-                            ALL
-                        </Tag>
-                        {skills.map(skill => (
-                            <Tag
-                                key={skill}
-                                closeable={false}
-                                kind="negative"
-                                variant={
-                                    filter === skill || filter === ''
-                                        ? 'solid'
-                                        : 'outlined'
-                                }
-                                onClick={
-                                    filter === skill
-                                        ? null
-                                        : () => setFilter(skill)
-                                }
-                            >
-                                {skill}
-                            </Tag>
-                        ))}
-                    </Block>
-                </Card>
-            </Container>
+                    <Card>
+                        <h3>Filter by Skill:</h3>
 
-            <Container width="1200px" padding="0 2rem 2rem">
-                {(filter
-                    ? categories
-                          .filter(c =>
-                              c.projects.some(p =>
-                                  p.skills.some(s => s === filter)
-                              )
-                          )
-                          .map(c => ({
-                              ...c,
-                              projects: c.projects.filter(p =>
-                                  p.skills.some(s => s === filter)
-                              ),
-                          }))
-                    : categories
-                ).map(category => (
-                    <React.Fragment key={category.name}>
-                        <T.H4>{category.name}</T.H4>
-                        <Block
-                            display="grid"
-                            gridTemplateColumns={[
-                                '1fr',
-                                '1fr',
-                                '1fr 1fr',
-                                '1fr 1fr 1fr',
-                            ]}
-                            gridGap="2rem"
-                        >
-                            {category.projects.map((project, i) => (
-                                <Block key={i} display="flex">
-                                    <WorkItem
-                                        work={project}
-                                        filter={filter}
-                                        setFilter={setFilter}
-                                    />
-                                </Block>
-                            ))}
-                        </Block>
-                    </React.Fragment>
-                ))}
-            </Container>
+                        <Tag>bootstrap</Tag>
+
+                    </Card>
+                </Container>
+            </Background>
         </Layout>
+        // <Layout backgroundColor="#ddd">
+        //     <T.H2 $style={{ textAlign: 'center' }}>My Work</T.H2>
+
+        //     <Container width="1200px" padding="0 2rem">
+        //         <Card>
+        //             <T.H5>Filter by Skill:</T.H5>
+        //             <Block
+        //                 $style={{
+        //                     textAlign: 'center',
+        //                     border: '0px solid red',
+        //                 }}
+        //             >
+        //                 <Tag
+        //                     closeable={false}
+        //                     kind="primary"
+        //                     variant={filter === '' ? 'solid' : 'outlined'}
+        //                     onClick={filter === '' ? null : () => setFilter('')}
+        //                     isFocused
+        //                 >
+        //                     ALL
+        //                 </Tag>
+        //                 {skills.map(skill => (
+        //                     <Tag
+        //                         key={skill}
+        //                         closeable={false}
+        //                         kind="negative"
+        //                         variant={
+        //                             filter === skill || filter === ''
+        //                                 ? 'solid'
+        //                                 : 'outlined'
+        //                         }
+        //                         onClick={
+        //                             filter === skill
+        //                                 ? null
+        //                                 : () => setFilter(skill)
+        //                         }
+        //                     >
+        //                         {skill}
+        //                     </Tag>
+        //                 ))}
+        //             </Block>
+        //         </Card>
+        //     </Container>
+
+        //     <Container width="1200px" padding="0 2rem 2rem">
+        //         {(filter
+        //             ? categories
+        //                   .filter(c =>
+        //                       c.projects.some(p =>
+        //                           p.skills.some(s => s === filter)
+        //                       )
+        //                   )
+        //                   .map(c => ({
+        //                       ...c,
+        //                       projects: c.projects.filter(p =>
+        //                           p.skills.some(s => s === filter)
+        //                       ),
+        //                   }))
+        //             : categories
+        //         ).map(category => (
+        //             <React.Fragment key={category.name}>
+        //                 <T.H4>{category.name}</T.H4>
+        //                 <Block
+        //                     display="grid"
+        //                     gridTemplateColumns={[
+        //                         '1fr',
+        //                         '1fr',
+        //                         '1fr 1fr',
+        //                         '1fr 1fr 1fr',
+        //                     ]}
+        //                     gridGap="2rem"
+        //                 >
+        //                     {category.projects.map((project, i) => (
+        //                         <Block key={i} display="flex">
+        //                             <WorkItem
+        //                                 work={project}
+        //                                 filter={filter}
+        //                                 setFilter={setFilter}
+        //                             />
+        //                         </Block>
+        //                     ))}
+        //                 </Block>
+        //             </React.Fragment>
+        //         ))}
+        //     </Container>
+        // </Layout>
     );
 };
 
