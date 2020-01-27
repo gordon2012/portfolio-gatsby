1f2b38485ba5712b1da499dfbd70dc97705a0307
Gordon Doskas <gordon2012@users.noreply.github.com>
2020-01-26 22:41:44 -0700

    Add styled to work-modal and carousel
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/components/work-modal.js
+++ src/components/work-modal.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Button } from 'baseui/button';
-import { Block } from 'baseui/block';
-// import { Modal, ModalHeader, ModalBody, FocusOnce } from 'baseui/modal';
+import { createPortal } from 'react-dom';
+import styled from 'styled-components';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import Image from './image';
 import Link from './link';
 import Carousel from './carousel';
 
-import styled from 'styled-components';
-
-
-// modal
-import { createPortal } from 'react-dom';
 const Modal = ({ children }) => {
     React.useLayoutEffect(() => {
         const {overflow, paddingRight} = window.getComputedStyle(document.body);
@@ -26,43 +20,68 @@ const Modal = ({ children }) => {
     }, []);
 
     const Container = styled.div`
-        /* border: 3px solid red; */
-
         z-index: 999;
-
         position: fixed;
-
         top: 0;
         left: 0;
-        /* width: 100vw; */
-
         width: 100%;
-
-        height: 100vh;
-
+        min-height: 100vh;
         display: flex;
         justify-content: center;
         align-items: center;
         background-color: rgba(0, 0, 0, 0.5);
-
     `;
 
     const Content = styled.div`
-        /* border: 3px solid lime; */
-
         width: 80%;
-        height: 90%;
-        /* border: 1px solid #d8dce3;
-        border-radius: 3px;
-        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.5);
-        background-color: #fdfdfe; */
-
+        min-height: 90%;
         background: white;
         box-shadow: 0 1px 4px rgba(0, 0, 0, 0.16);
-
-        padding: 1rem;
-
-
+        padding: 2rem;
+        display: flex;
+        flex-direction: column;
+        & > div:nth-child(2) {
+            flex: 1;
+        }
+        & > div:last-child {
+            display: flex;
+            @media (max-width: 599px) {
+                display: block;
+            }
+            div {
+                padding: 1rem;
+                flex: 1;
+                button {
+                    width: 100%;
+                    cursor: pointer;
+                    padding: 1rem;
+                    font-weight: bold;
+                    font-size: 1.2em;
+                    border: 3px solid #276ef1;
+                    background: transparent;
+                    color: #276ef1;
+                    :hover {
+                        background: #276ef1;
+                        color: white;
+                    }
+                }
+            }
+            div:last-child {
+                order: -1;
+                flex: 0;
+                button {
+                    border: 3px solid #e54937;
+                    color: #e54937;
+                    :hover {
+                        background: #e54937;
+                        color: white;
+                    }
+                }
+            }
+        }
+        h1 {
+            text-align: center;
+        }
     `;
 
     return createPortal(
@@ -75,110 +94,45 @@ const Modal = ({ children }) => {
     );
 };
 
+const Container = styled.div`
+    max-width: ${props => props.width};
+    margin: 0 auto;
+`;
+
 const WorkModal = ({ work, isOpen, setOpen }) => {
     return (
         isOpen && <Modal>
-            <h1>TITLE</h1>
-
             <div>
-
-
+                <h1>{work.title}</h1>
             </div>
-
-            <button onClick={() => setOpen(false)}>close</button>
+            <Container width="800px">
+                {documentToReactComponents(work.body.json)}
+                <br />
+                {work.images.length > 1 ? (
+                    <Carousel images={work.images}/>
+                ) : (
+                    <Container width="600px">
+                        <Image
+                            src={work.images[0].file.url}
+                            alt={work.images[0].title}
+                        />
+                    </Container>
+                )}
+                <br />
+            </Container>
+            <div>
+                {work.links.map((link, i) => (
+                    <div key={i}>
+                        <Link to={link.url} target="_blank" rel="noreferer noopener">
+                            <button>{link.title}</button>
+                        </Link>
+                    </div>
+                ))}
+                <div>
+                    <button onClick={() => setOpen(false)}>Close</button>
+                </div>
+            </div>
         </Modal>
-        // <Modal
-        //     size="auto"
-        //     onClose={() => setOpen(false)}
-        //     isOpen={isOpen}
-        //     display="flex"
-        //     flexDirection="column"
-        //     overrides={{
-        //         Root: {
-        //             style: {
-        //                 paddingTop: '1rem',
-        //                 paddingRight: '10vw',
-        //                 paddingBottom: '1rem',
-        //                 paddingLeft: '10vw',
-        //             },
-        //         },
-        //         Dialog: {
-        //             style: {
-        //                 display: 'flex',
-        //                 flexDirection: 'column',
-        //                 width: '100%',
-        //             },
-        //         },
-        //     }}
-        // >
-        //     <FocusOnce>
-        //         <span />
-        //     </FocusOnce>
-        //     <ModalHeader>{work.title}</ModalHeader>
-        //     <ModalBody $style={{ flex: '1' }}>
-        //         <Block maxWidth="800px" margin="0 auto" paddingBottom="2rem">
-        //             {documentToReactComponents(work.body.json)}
-        //         </Block>
-        //         {work.images.length > 1 ? (
-        //             <Block maxWidth="800px" margin="0 auto">
-        //                 <Carousel images={work.images} />
-        //             </Block>
-        //         ) : (
-        //             <Block maxWidth="600px" margin="0 auto">
-        //                 <Image
-        //                     src={work.images[0].file.url}
-        //                     alt={work.images[0].title}
-        //                 />
-        //             </Block>
-        //         )}
-        //     </ModalBody>
-        //     <Block
-        //         display={['block', 'flex']}
-        //         $style={{
-        //             padding: '0.75rem',
-        //         }}
-        //     >
-        //         {work.links.map((link, i) => (
-        //             <Block
-        //                 key={i}
-        //                 flex="1"
-        //                 $style={{
-        //                     padding: '0.75rem',
-        //                 }}
-        //             >
-        //                 <Link
-        //                     to={link.url}
-        //                     target="_blank"
-        //                     rel="noopener noreferrer"
-        //                     $style={{
-        //                         color: 'white',
-        //                         textDecoration: 'none',
-        //                     }}
-        //                 >
-        //                     <Button $style={{ width: '100%' }}>
-        //                         {link.title}
-        //                     </Button>
-        //                 </Link>
-        //             </Block>
-        //         ))}
-        //         <Block
-        //             padding="0.75rem"
-        //             flex={['1', '0']}
-        //             $style={{
-        //                 '@media (min-width: 600px)': {
-        //                     order: '-1',
-        //                 },
-        //             }}
-        //         >
-        //             <Button
-        //                 onClick={() => setOpen(false)}
-        //                 $style={{ width: '100%' }}
-        //             >
-        //                 Close
-        //             </Button>
-        //         </Block>
-        //     </Block>
-        // </Modal>
     );
 };
 
